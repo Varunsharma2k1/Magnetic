44270ec69ed41445ee29d40ea3451acc625873c3
Varunsharma2k1 <varunsharma@gmail.com>
2023-07-18 15:34:09 +0530

    data send

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+const axios = require('axios');
+var port = process.env.PORT || 8000;
+var cors = require('cors')
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var port = process.env.PORT || 8000;
+app.get('/api/data', (req, res) => {
+    // Fetch data from an external API or perform any other operations
+    const data = { message: 'Hello this is a connection of node and react' };
+    res.json(data);
+  });
+
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
